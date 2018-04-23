507bc4bd4c122761118932a5ea43d3be64cb7d5e
rgb-24bit <rgb-24bit@foxmail.com>
2018-04-23 21:17:13 +0800

    init: 更新配置
    设置垃圾回收隔一段时间会产生卡顿

--- init.el
+++ init.el
@@ -496,9 +496,9 @@ It should only modify the values of Spacemacs settings."
   (when (eq system-type 'windows-nt)
     (setq-default w32-system-coding-system 'cp936-dos)
     ;; https://emacs-china.org/t/topic/2900/3
-    (setq gc-cons-threshold (* 512 1024 1024))
-    (setq gc-cons-percentage 0.5)
-    (run-with-idle-timer 5 t #'garbage-collect)
+    ;; (setq gc-cons-threshold (* 512 1024 1024))
+    ;; (setq gc-cons-percentage 0.5)
+    ;; (run-with-idle-timer 5 t #'garbage-collect)
     ;; (setq garbage-collection-messages t)
     )
 
