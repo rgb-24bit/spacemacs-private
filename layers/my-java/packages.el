8ea8a67de61891e7399d1074bb8773e1a212d6be
rgb-24bit <rgb-24bit@foxmail.com>
2018-05-21 14:31:01 +0800

    layers: 准备添加 my-java

---
+++ layers/my-java/packages.el
@@ -0,0 +1,20 @@
+;;; packages.el --- my-java layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2018 rgb-24bit
+;;
+;; Author:  rgb-24bit <rgb-24bit@foxmail.com>
+;; URL: https://github.com/rgb-24bit/spacemacs-private
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Code:
+
+(defconst my-java-packages
+  '(
+    )
+  )
+
+
+;;; packages.el ends here
