205790f7f44b36f310d7c377506858557286e55b
rgb-24bit <rgb-24bit@foxmail.com>
2018-09-16 12:23:34 +0800

    custom.el

--- init.el
+++ init.el
@@ -606,6 +606,13 @@ in the dump."
   ;; javascript offset
   (setq-default js2-basic-offset 2)
   (setq-default js-indent-level 2)
+
+  ;; ===========================================================================
+  ;;                custom file config
+  ;; ===========================================================================
+
+  (setq custom-file (expand-file-name "custom.el" dotspacemacs-directory))
+  (load custom-file 'no-error 'no-message)
   )
 
 (defun dotspacemacs/emacs-custom-settings ()
@@ -613,27 +620,4 @@ in the dump."
 This is an auto-generated function, do not modify its content directly, use
 Emacs customize menu instead.
 This function is called at the very end of Spacemacs initialization."
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(disaster-project-root-files
-   (quote
-    (("Makefile")
-     ("setup.py" "package.json")
-     (".git/"))))
- '(evil-want-Y-yank-to-eol nil)
- '(org-agenda-files
-   (quote
-    ("~/Desktop/gtd/project.org" "~/Desktop/gtd/routine.org" "~/Desktop/gtd/task.org")))
- '(package-selected-packages
-   (quote
-    (ws-butler uuidgen string-inflection smartparens password-generator move-text lorem-ipsum link-hint hungry-delete expand-region eval-sexp-fu highlight editorconfig clean-aindent-mode aggressive-indent yasnippet-snippets yapfify yaml-mode winum which-key wgrep web-beautify volatile-highlights use-package unfill toc-org tagedit symon sqlup-mode sql-indent spaceline-all-the-icons smex restart-emacs realgud rainbow-delimiters pyvenv pytest py-isort prettier-js popwin plantuml-mode pippel pipenv pip-requirements pcre2el paradox overseer org-projectile org-present org-pomodoro org-mime org-download org-bullets org-brain open-junk-file nameless mwim mmm-mode markdown-toc macrostep live-py-mode json-navigator json-mode js2-mode js-doc ivy-yasnippet ivy-xref ivy-hydra indent-guide importmagic impatient-mode hl-todo highlight-parentheses highlight-numbers highlight-indentation helm-make groovy-mode graphviz-dot-mode google-c-style golden-ratio gnuplot gh-md fuzzy font-lock+ flx-ido fill-column-indicator fancy-battery evil-org elisp-slime-nav dotenv-mode doom-modeline disaster diminish cython-mode csharp-mode counsel company-statistics company-c-headers company-auctex company-anaconda column-enforce-mode color-theme-sanityinc-solarized cnfonts centered-cursor-mode bind-map auto-yasnippet auto-highlight-symbol auto-compile ace-window ace-link ac-ispell))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 )
