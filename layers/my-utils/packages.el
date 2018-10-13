50adc8900eaacf5a45ad8e440a92e6d14716933b
rgb-24bit <rgb-24bit@foxmail.com>
2018-10-13 11:35:49 +0800

    my-utils: add flex and bison

--- layers/my-utils/packages.el
+++ layers/my-utils/packages.el
@@ -1,12 +1,25 @@
 ;;; packages.el --- my-utils layer packages file for Spacemacs.
 
 (defconst my-utils-packages
-  '((insert-translated-name :location local))
-  )
+  '((insert-translated-name :location local)
+    (flex :location local)
+    (bison :location local)))
 
 (defun my-utils/init-insert-translated-name ()
   (use-package insert-translated-name
     :config
     (global-set-key (kbd "C-c i") 'insert-translated-name-insert)))
+
+(defun my-utils/init-flex ()
+  (use-package flex
+    :defer t
+    :mode ("\\.l$" . flex-mode)
+    :init (autoload 'flex-mode "flex")))
+
+(defun my-utils/init-bison ()
+  (use-package bison
+    :defer t
+    :mode ("\\.y$" . bison-mode)
+    :init (autoload 'bison-mode "bison")))
 
 ;;; packages.el ends here
