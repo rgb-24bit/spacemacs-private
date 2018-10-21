0826cfba73642226c111962f0bdf39bfd716f013
rgb-24bit <rgb-24bit@foxmail.com>
2018-10-21 22:28:23 +0800

    init: 更新配置

--- init.el
+++ init.el
@@ -86,7 +86,7 @@ This function should only modify configuration layer settings."
    ;; To use a local version of a package, use the `:location' property:
    ;; '(your-package :location "~/path/to/your-package/")
    ;; Also include the dependencies as they will not be resolved automatically.
-   dotspacemacs-additional-packages '(cnfonts)
+   dotspacemacs-additional-packages '(cnfonts edit-indirect)
 
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
