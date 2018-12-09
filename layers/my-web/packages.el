fae6260d31ae99f43ffcfb01801e9c11e507e76c
rgb-24bit <rgb-24bit@foxmail.com>
2018-12-09 15:41:53 +0800

    更新 my-web 层的配置

--- layers/my-web/packages.el
+++ layers/my-web/packages.el
@@ -1,44 +1,117 @@
 ;;; packages.el --- my-web layer packages file for Spacemacs.
 
 (defconst my-web-packages
-  '((mhtml-mode :location built-in)
+  '(
+    ;; common
     company
-    tagedit
-    emmet-mode
+    web-beautify
+
+    ;; for html
+    web-mode
+    (company-web :requires company)
+
+    ;; for css
     css-mode
     (css-sort-buffer :location local)
-    restclient
-    impatient-mode
-    web-beautify
+
+    ;; for javascript
     js2-mode
     js-doc
-    yasnippet
     )
   )
 
-(defun my-web/init-mhtml-mode ()
-  (use-package mhtml-mode
-    :defer t
-    :mode
-    (("\\.jsp\\'" . mhtml-mode)
-     ("\\.tmpl\\'" . mhtml-mode)
-     ("\\.djhtml\\'" . mhtml-mode))))
-
 (defun my-web/post-init-company ()
-  (spacemacs|add-company-backends
-    :backends company-files
-    :modes mhtml-mode)
   (spacemacs|add-company-backends
     :backends company-css
     :modes css-mode)
   (spacemacs|add-company-backends
     :backends company-capf
-    :modes js2-mode
-    ))
+    :modes js2-mode))
 
-(defun my-web/init-emmet-mode ()
-  (use-package emmet-mode
-    :defer t))
+(defun my-web/init-web-beautify ()
+  (use-package web-beautify
+    :defer t
+    :init
+    (progn
+      (spacemacs/set-leader-keys-for-major-mode 'web-mode "=" 'web-beautify-html)
+      (spacemacs/set-leader-keys-for-major-mode 'css-mode "=" 'web-beautify-css)
+      (spacemacs/set-leader-keys-for-major-mode 'js2-mode "=" 'web-beautify-js))))
+
+(defun my-web/init-web-mode ()
+  (use-package web-mode
+    :defer t
+    :config
+    (progn
+      ;; =======================================================================
+      ;; Spacemacs style keybingding
+      ;; =======================================================================
+
+      (spacemacs/declare-prefix-for-mode 'web-mode "mE" "errors")
+      (spacemacs/declare-prefix-for-mode 'web-mode "mg" "goto")
+      (spacemacs/declare-prefix-for-mode 'web-mode "mh" "dom")
+      (spacemacs/declare-prefix-for-mode 'web-mode "mr" "refactor")
+      (spacemacs/set-leader-keys-for-major-mode 'web-mode
+        "El" 'web-mode-dom-errors-show
+        "gb" 'web-mode-element-beginning
+        "gc" 'web-mode-element-child
+        "gp" 'web-mode-element-parent
+        "gs" 'web-mode-element-sibling-next
+        "hp" 'web-mode-dom-xpath
+        "rc" 'web-mode-element-clone
+        "rd" 'web-mode-element-vanish
+        "rk" 'web-mode-element-kill
+        "rr" 'web-mode-element-rename
+        "rw" 'web-mode-element-wrap
+        "z" 'web-mode-fold-or-unfold)
+
+      ;; =======================================================================
+      ;; web-mode theme
+      ;; =======================================================================
+
+      (set-face-attribute 'web-mode-doctype-face nil :foreground "#586e75")
+      (set-face-attribute 'web-mode-html-tag-face nil :foreground "#859900")
+      (set-face-attribute 'web-mode-html-tag-bracket-face nil :foreground "#586e75")
+      (set-face-attribute 'web-mode-html-attr-name-face nil :foreground "#268bd2")
+      (set-face-attribute 'web-mode-html-attr-value-face nil :foreground "#2aa198")
+      (set-face-attribute 'web-mode-html-attr-equal-face nil :foreground "#268bd2")
+      (set-face-attribute 'web-mode-html-tag-custom-face nil :foreground "#859900")
+
+      ;; =======================================================================
+      ;; others
+      ;; =======================================================================
+
+      ;; indentation settings
+      (setq web-mode-markup-indent-offset 2)
+
+      ;; colorization config
+      (setq web-mode-enable-css-colorization t))
+
+    :mode
+    (("\\.phtml\\'"      . web-mode)
+     ("\\.tpl\\.php\\'"  . web-mode)
+     ("\\.twig\\'"       . web-mode)
+     ("\\.xml\\'"        . web-mode)
+     ("\\.html\\'"       . web-mode)
+     ("\\.htm\\'"        . web-mode)
+     ("\\.[gj]sp\\'"     . web-mode)
+     ("\\.as[cp]x?\\'"   . web-mode)
+     ("\\.eex\\'"        . web-mode)
+     ("\\.erb\\'"        . web-mode)
+     ("\\.mustache\\'"   . web-mode)
+     ("\\.handlebars\\'" . web-mode)
+     ("\\.hbs\\'"        . web-mode)
+     ("\\.eco\\'"        . web-mode)
+     ("\\.ejs\\'"        . web-mode)
+     ("\\.djhtml\\'"     . web-mode)
+     ("\\.tmpl\\'"       . web-mode))))
+
+(defun my-web/init-company-web ()
+  (use-package company-web
+    :defer t
+    :init
+    (spacemacs|add-company-backends
+      :backends (company-web-html company-files)
+      :modes web-mode)))
 
 (defun my-web/init-css-mode ()
   (use-package css-mode
@@ -46,44 +119,6 @@
 
 (defun my-web/init-css-sort-buffer ()
   (use-package css-sort-buffer))
-
-(defun my-web/init-tagedit ()
-  (use-package tagedit
-    :defer t
-    :config
-    (progn
-      (tagedit-add-experimental-features)
-      (add-hook 'mthml-mode-hook (lambda() (tar-mode 1))))))
-
-(defun my-web/init-restclient ()
-  (use-package restclient
-    :defer t
-    :mode
-    (("\\.http\\'" . restclient-mode))))
-
-(defun my-web/init-impatient-mode ()
-  (use-package impatient-mode
-    :defer t
-    :init
-    (progn
-      (spacemacs/set-leader-keys-for-major-mode 'mhtml-mode "i" 'spacemacs/impatient-mode)
-      (spacemacs/set-leader-keys-for-major-mode 'css-mode "i" 'spacemacs/impatient-mode))
-    :config
-    (progn
-      ;; Alist indicating which filter should be used for which modes.
-      (setq imp-default-user-filters
-            '((html-mode . nil)
-              (mhtml-mode . nil)
-              (web-mode  . nil))))))
-
-(defun my-web/init-web-beautify ()
-  (use-package web-beautify
-    :defer t
-    :init
-    (progn
-      (spacemacs/set-leader-keys-for-major-mode 'mhtml-mode "=" ' web-beautify-html)
-      (spacemacs/set-leader-keys-for-major-mode 'css-mode "=" 'web-beautify-css)
-      (spacemacs/set-leader-keys-for-major-mode 'js2-mode "=" 'web-beautify-js))))
 
 (defun my-web/init-js2-mode ()
   (use-package js2-mode
@@ -110,10 +145,5 @@
     :config
     (setq js-doc-mail-address "rgb-24bit@foxmail.com"
           js-doc-author (format "rgb-24bit <%s>" js-doc-mail-address))))
-
-(defun my-web/pre-init-yasnippet ()
-  (spacemacs|use-package-add-hook yasnippet
-    :post-config
-    (yas-activate-extra-mode 'js-mode)))
 
 ;;; packages.el ends here
